65383b94f80f9f75fd9edd6028252aec76b4f4dd
Rayan Hantoul <ryan-055@outlook.com>
2023-05-08 21:43:36 +0300

    Solved

---
+++ Solved.py
@@ -0,0 +1,11 @@
+try:
+    def additoin(x, y):
+        x = 10
+        y = 20
+        print("Addition:", x + b)
+
+
+    additoin(10, 20)
+    print("the operation is successful")
+except NameError:
+    print("some variable is not defined")
