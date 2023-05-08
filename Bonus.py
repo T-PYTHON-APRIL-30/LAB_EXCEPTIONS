a76ad922224af10ffc1f1427fe2d4dac140de5d3
NouraAldayhani <nouraa.sultan9@gmail.com>
2023-05-08 14:52:13 +0300

    Solved the Lab Exceptions, Version 1

---
+++ Bonus.py
@@ -0,0 +1,14 @@
+def celsius_to_fahrenheit(celsius):
+    pass
+
+def fahrenheit_to_celsius(fahrenheit):
+    pass
+
+def split_phrase(phrase:str):
+    pass
+
+while True:
+
+    user_input= input('Enter a temperature and its unit (e.g., "25 C" or "77 F"):')
+    if "F" in user_input and "C" in user_input:
+        pass
