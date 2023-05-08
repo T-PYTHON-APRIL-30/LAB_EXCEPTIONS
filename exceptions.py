018d9e74aceee8e0e5f7a4dc9735576d36393339
Abdulaziz <Abdulaziz-A1997@hotmail.com>
2023-05-08 14:46:53 +0300

    exceptions solved

---
+++ exceptions.py
@@ -0,0 +1,10 @@
+try:
+    def additoin(x, y):
+        x = 10
+        y = 20
+        print("Addition:", x + b)
+    additoin(10, 20)
+except NameError:
+    print ("check the variable's name")
+else:
+    print ("operation successfully executed")
