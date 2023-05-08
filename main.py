a6faae1708b8e205225bbf97e66d00229ca35fb6
Dalia <daliaasq@gmail.com>
2023-05-08 14:47:02 +0300

    Exceptions Lab Completed

---
+++ main.py
@@ -0,0 +1,21 @@
+'''Below you have a code that raises an exception , using what you learned do the following:
+Find what type of exception is raised.
+Hanlde the exception in try..except
+If operation successful , print "the operation is successful"
+if operation fails, handle the specific exception that is raised , and print a relevant message.'''
+
+try:
+    def additoin(x, y):
+        x = 10
+        y = 20
+        print("Addition:", x + b)
+
+
+    additoin(10, 20)
+
+except NameError:
+    print("\ninvalid input, undefined variable\n")
+
+
+else:
+    print("\nthe operation is successful\n")
