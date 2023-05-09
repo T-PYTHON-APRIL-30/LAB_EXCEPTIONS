c8d99a1733e00033747a68c9f7f84016fd876761
juhaina <jh75n.awad@gmail.com>
2023-05-09 08:20:11 +0300

    Solve lab Exceptions!

---
+++ labExceptions.py
@@ -0,0 +1,12 @@
+def additoin(x, y):
+    x = 10
+    y = 20
+    #b = 20
+    try:
+        print("Result of Addition: ", x + b)
+    except Exception as e:
+        print("something went wrong. Try again later.")
+        print("save to file: ", e)
+    else:
+        print('the operation is successful')
+additoin(10, 20)
