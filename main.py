36931ac48eef95573ca124012da3b47bcf942313
SulimanBinM <sbinmohammed_1@icloud.com>
2023-05-10 16:35:18 +0300

    Fixed.

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ def additoin(x, y):
     x = 10
     y = 20
     try:
-        print("Addition:", x + y)
+        print("Addition:", x + b )
     except NameError:
         print("There is an invalid entry!!!")
     finally:
         print("The operation is successful")
 
 additoin(10, 20)
+
+#-------------- Bonus --------------
+
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
