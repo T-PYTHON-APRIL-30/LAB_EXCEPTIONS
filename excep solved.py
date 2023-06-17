12d66f05f73ab6dde39beaf5f514c7f48c7843f8
hess-01 <hessah-14@hotmail.com>
2023-06-17 13:34:07 +0300

    exceptions_lab0

--- excep solved.py
+++ excep solved.py
@@ -28,54 +28,29 @@ except NameError:
 # converter that accepts user input 
 # and converts temperatures between Celsius and Fahrenheit. 
 # You will handle various exceptions that might arise during the conversion process.
-
-#Instructions:
-#Write a function celsius_to_fahrenheit(celsius) that takes a Celsius temperature as an argument
-#  and returns the equivalent temperature in Fahrenheit. Use the formula fahrenheit = (celsius * 9/5) + 32.
-
-def celsius_to_fahrenheit(celsius):
-    fahrenheit = (celsius*9/5) + 32
-    return fahrenheit
-
-#Write a function fahrenheit_to_celsius(fahrenheit) that takes a Fahrenheit temperature as an argument 
-# and returns the equivalent temperature in Celsius. Use the formula celsius = (fahrenheit - 32) * 5/9.
+def celsius_to_fahrenheit(celsius : str):
+    celsius = int(celsius)
+    return (celsius * 9/5) + 32, "F"
 
 def fahrenheit_to_celsius(fahrenheit):
-    celsiue = (fahrenheit - 32) * 5/9
-    return celsius
+    fahrenheit = int(fahrenheit)
+    return (fahrenheit - 32) * 5/9, "C"
 
+def main():
+    temperature : tuple = tuple(input("please enter a temperature and its unit as (25 C or 77 F): ").split(" "))
+    tempValue, tempUnit = temperature
+    print(tempValue, tempUnit)
+    try:
 
-#Write a main function that: a. Prompts the user for input, asking them to enter a temperature
-# and its unit (either "C" for Celsius or "F" for Fahrenheit), separated by a space (e.g., "25 C" or "77 F"). b.
-# Splits the input string into a temperature value and its unit. c. Tries to convert the input temperature to its 
-# and prompt the user to try again. e. If the conversion is successful, print the converted temperature and its unit.
+        if tempUnit.upper() == "C":
+            tempValue, tempUnit = celsius_to_fahrenheit(tempValue)
+        elif tempUnit.upper() == "F":
+            tempValue, tempUnit = fahrenheit_to_celsius(tempValue)
+        else:
+            raise TypeError
+        print(tempValue, tempUnit)
+    except ValueError:
+        print("the input invalid please enter number")
+    except TypeError:
+        print("the unit is uncorrect")
 
-def main ():
-    while True:
-        try:
-            input_str = input ("Enter a temperture and its unit "(e.g., '25C' or '77 F'):")
-                               temp, unit = input_str.split()
-                               temp = float(temp)
-                               if unit == "C":
-                               converd_temp = celsius_to_fahrenheit(temp)
-                                       print(f"{temp} C is {converted_temp}F")
-         elif unit == "F":
-         converted_temp = fahrenheit_to_celsius(temp)
-         print(f"{temp}F is converted_temp}C")
-         else:
-         raiseTypeError("Invalid unit Please enter 'C' or 'F'.")
-         except ValueError:
-         print("Invalid temperture value. please try again")
-         except TypeError as e:
-         print(e)
-         else:
-            break
-
-
-
-
-#Call the main function to run the program. The user should be able to enter temperatures repeatedly
-#  until they enter a valid input.
-
-if__name__=="__main__":
-   main ()
